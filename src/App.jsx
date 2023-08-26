d6592c2bae4552dda4a07e0cb5738c613212cd4f
Bijay Bikram <becauseiam33@gmail.com>
2023-08-26 21:26:18 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  var number = 10
+
+  return (
+    <div className='cont'>
+      <h2>{10+10}</h2>
+      <h3>{number}</h3>
+      <h1 style={{color: 'red', background: 'black'}}>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
